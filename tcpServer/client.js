824366b9c7d52e5bf06fe00c4eb84fdfd60e2c9d
maksimShulga <maksim_shulga@ukr.net>
2016-03-31 12:21:42 +0300

    Additional task 3 was done.

--- tcpServer/client.js
+++ tcpServer/client.js
@@ -9,10 +9,17 @@ socket.connect({
   host: '127.0.0.1',
 }, function() {
     socket.write(JSON.stringify({ data: 'Hello from client' }));
-    socket.on('data', function(data) {
+    socket.on('data', function (data) {
         packet = JSON.parse(data);
         console.log('Data received (by client): ' + packet.data);
         if (Array.isArray(packet.data)) {
+            //console.log('client: ' + packet.clientId);
+            if (packet.clientId == 0) {
+                console.log('sleep client: ' + packet.clientId);
+                for(var i = 0; i < 5000000000; i++){
+                    //var a = 2 + 2;
+                }
+            }
             packet.data = packet.data.map(function (item) { return item *= 2; });
             console.log('Data to send: ' + packet.data);
             socket.write(JSON.stringify(packet));
--- tcpServer/server.js
+++ tcpServer/server.js
@@ -2,27 +2,41 @@ var api = {};
 global.api = api;
 api.net = require('net');
 
-var task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11, 6];
+var task1 = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11];
+var task2 = [22, 1, 14, 15, 9, 0, 11, 2, 17, 3, 2, 5, 7, 15];
 var result = [];
 var clients = [];
-var buffer = [];
-var count = 0;
-var numOfClients = 3;
+var buffer = [[],[]];
+var count = [0, 0];
+var isDoneTask = [0, 0];
+var nClientsForTask = [2, 2];
+var freeClients = 0;
 
-var server = api.net.createServer(function(socket) {
+var server = api.net.createServer(function (socket) {
+    clients.push({ socket: socket, id: clients.length, isBusy: 0});
+    freeClients++;
+    console.log('Number of clients: ' + clients.length);
+
+    zzz();
+
     console.log('Connected: ' + socket.localAddress);
     socket.on('data', function (data) {
         packet = JSON.parse(data);
-        console.log('Data received (by server): ' + packet.data);
+        console.log('Data received (by server): '+ packet.data);
         if (Array.isArray(packet.data)) {
-            buffer[packet.tId] = packet.data;
-            count++;
-            if (count == numOfClients) {
-                count = 0;
-                for (var i = 0; i < buffer.length; i++) {
-                    result.push.apply(result, buffer[i]);
+            clients[packet.clientId].isBusy = 0;
+            freeClients++;
+            isDoneTask[packet.taskId]++;
+            zzz();
+            buffer[packet.taskId][packet.id] = packet.data;
+            count[packet.taskId]++;
+            if (count[packet.taskId] == nClientsForTask[packet.taskId]) {
+                for (var i = 0; i < buffer[packet.taskId].length; i++) {
+                    result.push.apply(result, buffer[packet.taskId][i]);
                 }
-                console.log('Result data: ' + result);
+                console.log('Result data (task ' + (packet.taskId+1) + '): '+ result);
+                result = [];
+                buffer[packet.taskId] = [];
             }
         }
 
@@ -31,16 +45,43 @@ var server = api.net.createServer(function(socket) {
 
 server.on('connection', function (socket) {
 
-    clients.push(socket);
-    console.log('Number of clients: ' + clients.length);
 
-    if (clients.length == numOfClients) {
-        var sizeForClient = task.length / numOfClients;
+});
+
+var zzz = function () {
+    if (freeClients == nClientsForTask[0] && isDoneTask[0] == 0) {
+        var sizeForClient = task1.length / nClientsForTask[0];
         for (var i = 0; i < clients.length; i++) {
-            var packet = {};
-            packet.data = task.slice(i * sizeForClient, (i + 1) * sizeForClient);
-            packet.tId = i;
-            clients[i].write(JSON.stringify(packet));
+            if (clients[i].isBusy == 0) {
+                freeClients--;
+                var packet = {};
+                packet.data = task1.slice(i * sizeForClient, (i + 1) * sizeForClient);
+                packet.id = i;
+                packet.clientId = clients[i].id;
+                packet.taskId = 0;
+                clients[i].isBusy = 1;
+                clients[i].socket.write(JSON.stringify(packet));
+            }
         }
     }
-});
+    if (freeClients == nClientsForTask[1] && isDoneTask[1] == 0) {
+        var k = 0;
+        var sizeForClient = task2.length / nClientsForTask[1];
+        for (var i = 0; i < clients.length; i++) {
+            if (clients[i].isBusy == 0) {
+                freeClients--;
+                var packet = {};
+                packet.data = task2.slice(k * sizeForClient, (k + 1) * sizeForClient);
+                k++;
+                packet.id = i;
+                packet.clientId = clients[i].id;
+                packet.taskId = 1;
+                clients[i].isBusy = 1;
+                console.log("Client " + clients[i].id + ", gets " + packet.data);
+                clients[i].socket.write(JSON.stringify(packet));
+            }
+        }
+    }
+}
+
+
